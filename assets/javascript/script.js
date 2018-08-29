b115909c4876b6da68ea856123ebb861a59289df
Tom Schof <tom-schof@users.noreply.github.com>
2018-08-29 14:28:03 -0400

    javascript framework

---
+++ assets/javascript/script.js
@@ -0,0 +1,46 @@
+var q = $("#search-term").val().trim();
+var begin_date = $("#start-year").val().trim();
+var end_date = $("#end-year").val().trim();
+
+
+
+var params = {
+    'api-key': "69e810d940ea4558a3e8a2011b10c804",
+};
+
+if (q != null) {
+    params['q'] = q;
+}
+
+if (begin_date != null) {
+    params['begin_date'] = begin_date + "0101";
+}
+
+if (end_date != null) {
+    params['end_date'] = end_date + "0101";
+}
+
+
+console.log(params);
+
+var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+url += '?' + $.param(params);
+
+
+function searchNYT() {
+    $.ajax({
+        url: url,
+        method: 'GET',
+    }).done(function (result) {
+        console.log(result);
+    }).fail(function (err) {
+        throw err;
+    });
+
+
+}
+
+$("#search-articles").on("click", function (event) {
+    event.preventDefault();
+    searchNYT();
+});
